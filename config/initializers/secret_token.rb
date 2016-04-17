d5270c2f9b7e0f6951558c5b022b8a61026581dd
Jenn Eng <Jenn@Jennifers-MacBook-Pro-2.local>
2016-04-17 12:36:02 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+BackBurner::Application.config.secret_key_base = '791dd36d309f85308c724c518497a6a5e6fa71a69fcaeaf95a0ec56082ebb1e54a3be0639d7295e06fa0335063331df5d0b118cd235bf4d7bc2e5fdd6f6afec9'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+BackBurner::Application.config.session_store :cookie_store, key: '_back_burner_session'
