ab57aae9c72a0f3cc08af7af8ce21e77b808edee
Jenn Eng <Jenn@Jennifers-MacBook-Pro-2.local>
2016-04-17 18:05:23 -0400

    attempt at customizing registrations controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,17 +4,18 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   # protect_from_forgery with: :exception
-  before_filter :configure_permitted_parameters, if: :devise_controller?
+  # before_filter :configure_permitted_parameters, if: :devise_controller?
 
   protected
 
-  def configure_permitted_parameters
-    if resource_class == Chef
-      devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:first_name, :last_name, :email, :restaurant_name, :address, :city, :state, :zip, :phone_number, :description, :password, :password_confirmation)}
-    end
-  end
+  # def configure_permitted_parameters
+  #   if resource_class == Chef
+  #     devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:first_name, :last_name, :email, :restaurant_name, :address, :city, :state, :zip, :phone_number, :description, :password, :password_confirmation)}
+  #   end
+  # end
 
   def after_sign_in_path_for(resource)
+    # flash[:success] = "Great Success!"
     posts_path
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 BackBurner::Application.routes.draw do
-  devise_for :chefs
+  # devise_for :chefs
   root "welcome#index"
+  devise_for :chefs, controllers: {
+        :registrations => "chef/registrations"
+      }
 
   resources :posts, only: [:index]
 end
