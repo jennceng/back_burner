92f25d4c879875f3f549b974a1b9739be8887163
Jenn Eng <Jenn@Jennifers-MacBook-Pro-2.local>
2016-04-18 15:10:27 -0400

    feed hound

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,5 +5,5 @@ class Post < ActiveRecord::Base
   validates :date, presence: true
   validates :start_time, presence: true
   validates :end_time, presence: true
-  validates :wage, presence: true 
+  validates :wage, presence: true
 end
--- spec/factories/posts.rb
+++ spec/factories/posts.rb
@@ -1,6 +1,6 @@
 FactoryGirl.define do
   factory :post do
-    sequence(:date) { |n| "May #{n}, 2016"}
+    sequence(:date) { |n| "May #{n}, 2016" }
     start_time "10:00 am"
     end_time "11:00 pm"
     wage "$10"
