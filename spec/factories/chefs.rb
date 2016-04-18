5bb850fcbd352b4e3d6d2cd0a37e77f21f69f85b
Jenn Eng <Jenn@Jennifers-MacBook-Pro-2.local>
2016-04-18 11:43:03 -0400

    add factory for chef and test for updating account info

--- spec/factories/chefs.rb
+++ spec/factories/chefs.rb
@@ -1,5 +1,15 @@
 FactoryGirl.define do
   factory :chef do
-    
+    first_name "Dan"
+    last_name "Taft"
+    restaurant_name "Poutine for Dayz"
+    address "33 Harrison Avenue"
+    city "Boston"
+    state "MA"
+    zip "02111"
+    description "homemade poutine"
+    phone_number "5555555555"
+    sequence(:email) { |n| "DTizzle#{n}@hotmail.com" }
+    password "password"
   end
 end
