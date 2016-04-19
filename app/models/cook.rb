71bcc398c8261c239c0383e043cd2e9f3ac34d4d
Jenn Eng <Jenn@Jennifers-MacBook-Pro-2.local>
2016-04-19 15:48:14 -0400

    update account tests

--- app/models/cook.rb
+++ app/models/cook.rb
@@ -3,4 +3,9 @@ class Cook < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :email, presence: true
+  validates :phone_number, presence: true
 end
